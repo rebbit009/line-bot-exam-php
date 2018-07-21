bd273ebd9af8ff3b23a54501cca61bd850d7aad6
rebbit009 <41474019+rebbit009@users.noreply.github.com>
2018-07-21 21:07:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'qNueVydzXEQaUyrXFcFZB2sFLFo8OfnRCr+FNlO7itPAIRWeHBlLIp0zvxBdA+rY/YdDUp8oSA+Qsvzde3zUe62Ay0ZRaBQileC5NVOphpxGpBrg+N4pW3yN129GUi77nnUqVEVLAetVZANX4Px9sgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
